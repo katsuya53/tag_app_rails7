74e5c0ac2c05878b73b9218c8682825e2480a85e
katsuya53 <redstar53hk@gmail.com>
2024-04-14 14:36:48 +0900

    編集機能の実装

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -2,7 +2,10 @@ class PostForm
   include ActiveModel::Model
 
   #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
-  attr_accessor :text, :image
+  attr_accessor(
+    :text, :image,
+    :id, :created_at, :updated_at
+   )
 
   with_options presence: true do
     validates :text
@@ -12,4 +15,9 @@ class PostForm
   def save
     Post.create(text: text, image: image)
   end
+
+  def update(params, post)
+    post.update(params)
+  end
+
 end
